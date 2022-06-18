c2b2e434145b22b74d3b29eb86c954972eec8200
sbirvin1s <sbirvin1s@gmail.com>
2022-06-18 16:13:41 -0500

    adds full server POST and GET support

--- server/request-handler.js
+++ server/request-handler.js
@@ -15,93 +15,50 @@ this file and include it in basic-server.js so that it actually works.
 ---------------------------- PROJECT NOTES ---------------------------
 1. the requestHandler constructor is written in functional instantiation style - BE CONSISTENT!!!!
 2. Need additional functions to check for port name, parts of request
-3.
-*/
-
-var requestHandler = function (request, response) {
-  // debugger;
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
-
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
-
-  // complete url: 'http://127.0.0.1:3000/classes/messages'
-
-  // The outgoing status.
-  var statusCode = 200;
-
-
-  // See the note below about CORS headers.
-
-  var defaultCorsHeaders = {
-    'access-control-allow-origin': '*',
-    'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-    'access-control-allow-headers': 'content-type, accept, authorization',
-    'access-control-max-age': 10 // Seconds.
-  };
-
-  var headers = defaultCorsHeaders;
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
-
-  //GET request response
-  //data, url, and type are the most important pieces
-  //POST requeset response
-  if (!request.url === 'http://127.0.0.1:3000/classes/messages') {
-    if (request.method === 'GET') {
-      response.writeHead(404,);
-      //console log in here to see if the server is receiving this kind of thing
-      // console.log('This Works');
-      response.end('Hello, World!');
-      // console.log('This Works');
-    }
-    if (request.method === 'POST') {
-      // console.log('This Works');
-      response.end('Created');
-
-    }
-  } else if (request.url === 'http://127.0.0.1:3000/classes/messages') { // <---------------- THIS is the request URL!!!!)
-    // console.log('This Works');
-    response.end('Testing!');
+3. if (request.url !== ‘/classes/messages’) {
+    response.writeHead(notFoundCode, headers);
+    response.end(‘404 not found’);
   }
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-  response.end('Hello, World!');
+*/
+var defaultCorsHeaders = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept, authorization',
+  'access-control-max-age': 10 // Seconds.
 };
 
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
+var messages = [];
+
+var requestHandler = function (request, response) {
+
+  console.log('Serving request type ' + request.method + ' for url ' + request.url);
+
+  var headers = defaultCorsHeaders;
+  headers['Content-Type'] = 'application/JSON';
+
+
+
+  if (!(request.url === '/classes/messages')) {
+    if (request.method === 'GET') {
+      response.writeHead(404, headers);
+      response.end('Not Found');
+    }
+  } else if (request.url === '/classes/messages') {
+    if (request.method === 'GET') {
+      response.writeHead(200, headers);
+      response.end(JSON.stringify(messages));
+
+    }
+    if (request.method === 'POST') {
+      request.on('data', (message) => {
+        messages.push(JSON.parse(message));
+      });
+      response.writeHead(201, headers);
+      response.end('This Posted');
+    }
+  }
+};
+
 
 module.exports = {
   requestHandler
